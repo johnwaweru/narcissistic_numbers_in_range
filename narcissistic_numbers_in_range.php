f3540116259397a2d7a1c16dd19881d00048f7f1
John Waweru <john@johnwaweru.com>
2015-04-23 11:51:43 +0300

    Modified narcissistic numbers oin range

--- narcissistic_numbers_in_range.php
+++ narcissistic_numbers_in_range.php
@@ -3,5 +3,20 @@
    // Author: John Waweru
    // Website: www.johnwaweru.com
    // Email: john@johnwaweru.com
+
+	for($i =1; $i <= 100000; $i++) {
+		$n = $i;
+		$sum = 0;
+		
+		while($n != 0) {
+			$r = $n%10; // Extract the last digit
+			$sum = $sum + pow($r, strlen($i));
+			$n = $n/10; // Reduce number by 1
+		}
+		
+		if($sum == $i) {
+			echo $i . "<br />";
+		}
+	}
    
 ?>

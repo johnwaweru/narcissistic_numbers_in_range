923a03a8ea39a30ba6ae36754ed97f6fc956d898
John Waweru <john@johnwaweru.com>
2015-04-23 11:53:50 +0300

    Displays narcissistic/armstrong numbers between 1 and 1000

--- narcissistic_numbers_in_range.php
+++ narcissistic_numbers_in_range.php
@@ -1,22 +1,21 @@
 <?php
-   // Displays narcissistic/armstrong numbers between 1 and 1000
-   // Author: John Waweru
-   // Website: www.johnwaweru.com
-   // Email: john@johnwaweru.com
+// Displays narcissistic/armstrong numbers between 1 and 1000
+// Author: John Waweru
+// Website: www.johnwaweru.com
+// Email: john@johnwaweru.com
 
-	for($i =1; $i <= 100000; $i++) {
-		$n = $i;
-		$sum = 0;
-		
-		while($n != 0) {
-			$r = $n%10; // Extract the last digit
-			$sum = $sum + pow($r, strlen($i));
-			$n = $n/10; // Reduce number by 1
-		}
-		
-		if($sum == $i) {
-			echo $i . "<br />";
-		}
+for($i =1; $i <= 1000; $i++) {
+	$n = $i;
+	$sum = 0;
+	
+	while($n != 0) {
+		$r = $n%10; // Extract the last digit
+		$sum = $sum + pow($r, strlen($i));
+		$n = $n/10; // Reduce number by 1
 	}
-   
+	
+	if($sum == $i) {
+		echo $i . "<br />";
+	}
+}
 ?>

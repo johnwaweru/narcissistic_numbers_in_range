4024d07b50d20c2cd8bc66d86fbc05e67ab27939
johnwaweru <johnkamauwaweru@gmail.com>
2019-09-17 19:39:05 +0300

    Update narcissistic_numbers_in_range.php

--- narcissistic_numbers_in_range.php
+++ narcissistic_numbers_in_range.php
@@ -1,8 +1,6 @@
 <?php
 // Displays narcissistic/armstrong numbers between 1 and 1000
 // Author: John Waweru
-// Website: www.johnwaweru.com
-// Email: john@johnwaweru.com
 
 for($i =1; $i <= 1000; $i++) {
 	$n = $i;
